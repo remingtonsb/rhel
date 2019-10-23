7f1beb3ec56082ed4a218f2bfbacb4896e002be1
Remington Santos <remingtonsb@gmail.com>
2019-10-23 18:16:28 -0300

    Create manage_users.sh

---
+++ manage_users.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Please write you action 'add' to add user or 'del' to del user"
+  read ACTION_STRING
+  case $ACTION_STRING in
+	add)
+		echo "adding users"
+		for i in $(cat lista_users);do sudo adduser $i;done
+		echo "checking users "
+		for i in $(cat lista_users);do sudo cat /etc/passwd | grep $i > /dev/null && echo OK $i criado com sucesso || echo ERROR usuario $i nao criado;done
+		echo "Defining password users"
+		for i in $(cat lista_users);do sudo echo redhat | sudo passwd $i --stdin; done
+		;;
+	del)
+		echo "Deleting users"
+		for i in $(cat lista_users);do sudo userdel -r -f $i;done
+		;;
+	*)
+		echo "Use add to add users or del to del users"
+		;;
+  esac
