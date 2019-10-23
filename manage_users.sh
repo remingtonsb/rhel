09846e7c4789e783c9722ed916359a8aefa21b0e
Remington Santos <remingtonsb@gmail.com>
2019-10-23 18:47:40 -0300

    Update manage_users.sh

--- manage_users.sh
+++ manage_users.sh
@@ -10,6 +10,8 @@ echo "Please write you action 'add' to add user or 'del' to del user"
 		for i in $(cat lista_users);do sudo cat /etc/passwd | grep $i > /dev/null && echo OK $i criado com sucesso || echo ERROR usuario $i nao criado;done
 		echo "Defining password users"
 		for i in $(cat lista_users);do sudo echo redhat | sudo passwd $i --stdin; done
+		echo "add users to group testdrive"
+		for i in $(cat lista_users);do sudo usermod -g testdrive $i; done
 		;;
 	del)
 		echo "Deleting users"
